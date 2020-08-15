26b24e62db413959b834363ae8eb8b10ca94f7fe
FunHaver <conortsullivan@gmail.com>
2020-08-15 20:51:16 +0000

    Added README Detail and eliminated cruft

--- MrBuddy.py
+++ MrBuddy.py
@@ -1,8 +1,6 @@
 from discord.ext.commands import Bot
 import asyncio
 import aiohttp
-import json
-import random
 import subprocess
 
 BOT_TOKEN = ''
@@ -15,14 +13,9 @@ async def on_message(message):
         return
     if 'Hey MrBuddy' in message.content:
         trimmed_message = message.content[12:]
-        escaped_message = ""
-        for i in range(0, len(trimmed_message)):
-            if trimmed_message[i] == " ":
-                trimmed_message += ""
-            escaped_message += trimmed_message[i]
-        print(escaped_message)
+        print(trimmed_message)
         
-        subprocess.run(["/home/conor/DiscordBot/execute-gpt2.sh", escaped_message])
+        subprocess.run(["/home/conor/DiscordBot/execute-gpt2.sh", trimmed_message])
         generated_text = open("output.txt", "r")
         await message.channel.send(generated_text.read())
 
--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # MrBuddy
 My Dumb Discord Bot
 
-## Requirements
-* gpt-2
-* Discord.py
+## Dependancies
+* gpt-2 [repo here](https://github.com/openai/gpt-2)
+* Discord.py [repo here](https://github.com/Rapptz/discord.py)
